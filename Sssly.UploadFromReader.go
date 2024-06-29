86e9fa1211ae82b3d3ea1e3e4d979c9f6f8d1dbb
Luis Otavio de Colla Furquim <luisfurquim@gmail.com>
2024-06-28 21:19:39 -0300

    @{newfeauture} New method UploadFromReader

--- Sssly.UploadFromReader.go
+++ Sssly.UploadFromReader.go
@@ -1,23 +1,21 @@
 package sssly
 
 import (
-	"os"
+	"io"
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func (s *Sssly) Upload(key, fname string) error {
-	var fh *os.File
+func (s *Sssly) UploadFromReader(key string, rd io.Reader) error {
 	var err error
 
-	fh, err = os.Open(fname)
-	if err != nil {
-		Goose.Storage.Logf(1, "Error opening %s for read: %s", fname, err)
-		return err
-	}
+	_, err = s.Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(s.BasePath + key),
+		Body:   rd,
+	})
 
-	err = s.UploadFromReader(key, fh)
 	if err != nil {
 		Goose.Storage.Logf(1, "Error opening %s for write: %s", key, err)
 	}
