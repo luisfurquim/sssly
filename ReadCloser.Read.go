f4b0ab900d231c530444d625d5bcc5666872c08b
Luis Otavio de Colla Furquim <luisfurquim@gmail.com>
2025-10-01 04:07:11 -0300

    @{newfeature}Added multipart upload and download support.

--- ReadCloser.Read.go
+++ ReadCloser.Read.go
@@ -18,7 +18,7 @@ func (rc *ReadCloser) Read(buf []byte) (int, error) {
 	Goose.Storage.Logf(4, "Going to read: %d, consumed: %d", rc.chunk, rc.consumed)
 
 	if rc.remReader == nil {
-		Goose.Storage.Logf(0, "Obtaining remote reader")
+		Goose.Storage.Logf(3, "Obtaining remote reader")
 
 		resp, err = rc.cli.Client.GetObject(
 			context.TODO(),
@@ -44,7 +44,7 @@ func (rc *ReadCloser) Read(buf []byte) (int, error) {
 			return 0, io.EOF
 		}
 
-		Goose.Storage.Logf(0, "Fetching new chunk: %d", rc.chunk)
+		Goose.Storage.Logf(3, "Fetching new chunk: %d", rc.chunk)
 
 		for sz<len(rc.buffer) && err==nil {
 			Goose.Storage.Logf(4, "sz: %d", sz)
@@ -56,34 +56,34 @@ func (rc *ReadCloser) Read(buf []byte) (int, error) {
 			sz += n
 		}
 
-		Goose.Storage.Logf(0, "sz=%d", sz)
+		Goose.Storage.Logf(4, "sz=%d", sz)
 		rc.consumed = 0
 		sz -= 38
 		rc.rd = bytes.NewReader(rc.buffer[8:sz])
 //		rc.rd = bytes.NewReader(rc.buffer[8:rc.chunkSize+8])
-		Goose.Storage.Logf(0, "Removing header and trailer: %d % 2x .. % 2x .. % 2x", rc.chunk, rc.buffer[:8], rc.buffer[8:16], rc.buffer[sz:sz+16])
-		Goose.Storage.Logf(0, "Removing header and trailer: %d %s .. %s .. %s", rc.chunk, rc.buffer[:8], rc.buffer[8:16], rc.buffer[sz:sz+16])
+		Goose.Storage.Logf(5, "Removing header and trailer: %d % 2x .. % 2x .. % 2x", rc.chunk, rc.buffer[:8], rc.buffer[8:16], rc.buffer[sz:sz+16])
+		Goose.Storage.Logf(5, "Removing header and trailer: %d %s .. %s .. %s", rc.chunk, rc.buffer[:8], rc.buffer[8:16], rc.buffer[sz:sz+16])
 	}
 
 	n, err = rc.rd.Read(buf)
 	rc.consumed += int32(n)
 	Goose.Storage.Logf(4, "Read %d bytes: %s", n, err)
 	if err == io.EOF {
-		Goose.Storage.Logf(0,"EOF")
+		Goose.Storage.Logf(3,"EOF")
 		if rc.chunk < rc.chunks {
-			Goose.Storage.Logf(0,"rc.chunk %d < rc.chunks %d", rc.chunk, rc.chunks)
-			err = nil
+			Goose.Storage.Logf(3,"rc.chunk %d < rc.chunks %d", rc.chunk, rc.chunks)
 			if n == 0 {
-				Goose.Storage.Logf(0,"n==0 panic")
-				panic("Stopped!")
+				Goose.Storage.Logf(1,"Incomplete reading!")
+				return 0, err
 			}
+			err = nil
 		}
 	} else if err != nil {
 		Goose.Storage.Logf(1, "Error reading %s on chunk[%d]: %s", rc.key, rc.chunk, err)
 		return 0, err
 	}
 
-	Goose.Storage.Logf(0,"done reading %d bytes", n)
+	Goose.Storage.Logf(4,"done reading %d bytes", n)
 
 	return n, err
 }
